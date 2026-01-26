bad99f7f984859c769ed78689d6d73d11597d873
Mohammad Zaid Khursheed <iamratedr4@gmail.com>
2026-01-26 23:37:53 +0530

    Day 1js

---
+++ Day1.js
@@ -0,0 +1,13 @@
+// Day 1 JavaScript Practice
+
+let name = "Zaid";
+let age = 23;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+function greet(user) {
+  return "Hello " + user;
+}
+
+console.log(greet(name));
